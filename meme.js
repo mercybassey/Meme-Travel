7e16c69f37bfc6f41aba5688b261703a187c3911
mercybassey <udohmercy911@gmail.com>
2021-08-08 13:06:15 +0000

    Moved 'fortune cookie' functionality into module.

--- meme.js
+++ meme.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fortune = require('./lib/fortune');
 
 const app = express();
 
@@ -16,18 +17,8 @@ app.get('/', (req,res) => {
 });
 
 app.get('/about', (req,res) => {
-    const randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
-    res.render('about', {fortune:randomFortune});
+    res.render('about', {fortune:fortune.getFortune()});
 });
-
-// fortune cookies
-const fortunes = [
-    "Conquer your fears or they will conquer you.",
-    "Rivers need springs.",
-    "Do not fear what you don't know.",
-    "You will have a pleasant surprise.",
-    "Whenever possible, keep it simple.",
-]
 
 // express custom 404 page
 app.use((req,res, next) => {
